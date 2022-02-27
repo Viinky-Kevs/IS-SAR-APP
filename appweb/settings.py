e8a33efa1fae18ad11cea5872279d3daf923d9cb
Kevs <84353351+Viinky-Kevs@users.noreply.github.com>
2022-02-26 21:52:02 -0500

    Update settings.py

--- appweb/settings.py
+++ appweb/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t23q)%60#1s05e%@1ly4($)xuz0(_7^(3_g2(q5uo6q^43!y&9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['137.184.239.220', 'localhost']
+ALLOWED_HOSTS = ['161.35.226.71', 'localhost']
 #'www.is-sar.com'
 
 
