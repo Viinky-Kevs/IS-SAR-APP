917c347bcc7cde14809ceed543fdfc86abc6326f
Kevs <84353351+Viinky-Kevs@users.noreply.github.com>
2022-03-10 18:25:28 -0500

    Update settings.py

--- appweb/settings.py
+++ appweb/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-t23q)%60#1s05e%@1ly4($)xuz0(_7^(3_g2(q5uo6q^43!y&9
 DEBUG = False
 #DEBUG = True
 #ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['www.is-sar.com', '164.92.71.106', 'localhost']
+ALLOWED_HOSTS = ['www.is-sar.com', '128.199.5.110', 'localhost']
 
 # Application definition
 
