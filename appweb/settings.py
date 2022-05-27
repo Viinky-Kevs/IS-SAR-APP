226e06dbb893251871f71e6920759f286bca9999
Kevs <84353351+Viinky-Kevs@users.noreply.github.com>
2022-05-26 23:24:35 -0500

    Commit

--- appweb/settings.py
+++ appweb/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-t23q)%60#1s05e%@1ly4($)xuz0(_7^(3_g2(q5uo6q^43!y&9
 DEBUG = False
 #DEBUG = True
 #ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['www.is-sar.com', '146.190.26.203', 'localhost']
+ALLOWED_HOSTS = ['www.is-sar.com', '159.223.15.244', 'localhost']
 
 INSTALLED_APPS = [
     'GEEMAP',
